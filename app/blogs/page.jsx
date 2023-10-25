3c56d1af946d8605d4aabfebc33377307fbb9bbf
sufi0900 <108760976+sufi0900@users.noreply.github.com>
2023-10-26 02:12:42 +0500

    New Commit

--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -6,6 +6,7 @@ import Grid from "@mui/material/Grid";
 import Head from "next/head";
 import Script from "next/script";
 import { NextSeo } from "next-seo";
+import Body from "./nextTheme";
 export const metadata = {
   title: "Blogs - Sufian Mustafa",
   description:
@@ -42,95 +43,97 @@ export default async function AllBlogs() {
   const data = await getData();
   return (
     <div style={{ minHeight: "100vh" }}>
-      <Head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Blogs - Sufian Mustafa</title>
-        <meta
-          name="description"
-          content="Welcome to the portfolio of SuFiaN MusTaFa, a passionate web developer based in Pakistan. Explore my projects and skills in web development. I have a wide range of experience in web development, with a
+      <Body>
+        <Head>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Blogs - Sufian Mustafa</title>
+          <meta
+            name="description"
+            content="Welcome to the portfolio of SuFiaN MusTaFa, a passionate web developer based in Pakistan. Explore my projects and skills in web development. I have a wide range of experience in web development, with a
       focus on front-end technologies such as HTML, CSS, and
       JavaScript with a particular focus on ReactJS NextJS"
-        />
-        <meta name="author" content="Sufian Mustafa" />
-        <meta property="og:title" content="Blogs - Sufian Mustafa" />
-        <meta
-          property="og:description"
-          content="Explore Sufian Mustafa's blog for insightful articles and updates on web development, technology, and more.."
-        />
-        <meta
-          property="og:image"
-          content="https://res.cloudinary.com/dtvtphhsc/image/upload/v1694356123/Sufian-Mustafa-Web-Developer_en5jxl.png"
-        />
-        <meta property="og:url" content="https://sufianmustafa.com/blogs" />
-        <link rel="canonical" href="https://sufianmustafa.com/blogs" />
-        <NextSeo
-          title="Blogs - Sufian Mustafa"
-          description="Explore Sufian Mustafa's blog for insightful articles and updates on web development, technology, and more."
-          author="Sufian Mustafa"
-          canonical="https://sufianmustafa.com/blogs"
-          openGraph={{
-            title: "Blogs - Sufian Mustafa",
-            description:
-              "Explore Sufian Mustafa's blog for insightful articles and updates on web development, technology, and more.",
-            type: "Blog",
-            url: "https://sufianmustafa.com/blogs",
-            images: [
-              {
-                url: "https://res.cloudinary.com/dtvtphhsc/image/upload/v1697725757/Screenshot_249_edsr2z.png",
-                width: 800,
-                height: 800,
-                alt: "web developer Image",
-              },
-            ],
-          }}
-        />
-      </Head>
-      <Script
-        id="MyBlogs-webpage-ld-json"
-        type="application/ld+json"
-        dangerouslySetInnerHTML={sufianmustafawebdeveloperblogs()}
-        key="MyBlogs-jsonld"
-      />
-      <br></br>
-      <br></br>
-      <div className="about41" style={{ marginTop: "60px" }}>
-        <h3>
-          My Blogs
-          <span
-            style={{
-              float: "right",
-              marginTop: "5px",
+          />
+          <meta name="author" content="Sufian Mustafa" />
+          <meta property="og:title" content="Blogs - Sufian Mustafa" />
+          <meta
+            property="og:description"
+            content="Explore Sufian Mustafa's blog for insightful articles and updates on web development, technology, and more.."
+          />
+          <meta
+            property="og:image"
+            content="https://res.cloudinary.com/dtvtphhsc/image/upload/v1694356123/Sufian-Mustafa-Web-Developer_en5jxl.png"
+          />
+          <meta property="og:url" content="https://sufianmustafa.com/blogs" />
+          <link rel="canonical" href="https://sufianmustafa.com/blogs" />
+          <NextSeo
+            title="Blogs - Sufian Mustafa"
+            description="Explore Sufian Mustafa's blog for insightful articles and updates on web development, technology, and more."
+            author="Sufian Mustafa"
+            canonical="https://sufianmustafa.com/blogs"
+            openGraph={{
+              title: "Blogs - Sufian Mustafa",
+              description:
+                "Explore Sufian Mustafa's blog for insightful articles and updates on web development, technology, and more.",
+              type: "Blog",
+              url: "https://sufianmustafa.com/blogs",
+              images: [
+                {
+                  url: "https://res.cloudinary.com/dtvtphhsc/image/upload/v1697725757/Screenshot_249_edsr2z.png",
+                  width: 800,
+                  height: 800,
+                  alt: "web developer Image",
+                },
+              ],
             }}
-            className="searchicon0"
-          >
-            {" "}
-          </span>
-        </h3>
-      </div>
-      <div>
+          />
+        </Head>
+        <Script
+          id="MyBlogs-webpage-ld-json"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={sufianmustafawebdeveloperblogs()}
+          key="MyBlogs-jsonld"
+        />
         <br></br>
         <br></br>
-        <div>
-          <div>
-            <Grid
-              container
+        <div className="about41" style={{ marginTop: "60px" }}>
+          <h3>
+            My Blogs
+            <span
               style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                textAlign: "center",
+                float: "right",
+                marginTop: "5px",
               }}
+              className="searchicon0"
             >
-              {data.map((post) => (
-                <BlogCard key={post._id} {...post} />
-              ))}
-            </Grid>
-            <br />
-            <br />
+              {" "}
+            </span>
+          </h3>
+        </div>
+        <div>
+          <br></br>
+          <br></br>
+          <div>
+            <div>
+              <Grid
+                container
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  textAlign: "center",
+                }}
+              >
+                {data.map((post) => (
+                  <BlogCard key={post._id} {...post} />
+                ))}
+              </Grid>
+              <br />
+              <br />
+            </div>
           </div>
         </div>
-      </div>
+      </Body>
     </div>
   );
 }
