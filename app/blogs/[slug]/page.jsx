217a86500443d6ba6d806976627b11c2e6cc9ed3
sufi0900 <108760976+sufi0900@users.noreply.github.com>
2023-10-26 18:19:58 +0500

    New Commit

--- app/blogs/[slug]/page.jsx
+++ app/blogs/[slug]/page.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { client } from "@/app/lib/sanity";
 import { urlFor } from "@/app/lib/sanityImageUrl";
-
+import Themeitem from "./Themeitem";
 import Grid from "@mui/material/Grid";
 import { PortableText } from "@portabletext/react";
 import NextTheme from "../nextTheme";
@@ -12,7 +12,6 @@ import "animate.css/animate.min.css";
 import Divider from "@mui/material/Divider";
 import Head from "next/head";
 import Script from "next/script";
-import BlogDetailSection from "./blogdetail";
 
 async function getData(slug) {
   const query = `*[_type == "post" && slug.current == "${slug}"][0]`;
@@ -77,10 +76,78 @@ export default async function BlogCardDetail({ params }) {
             xs={11}
             style={{ overflow: "hidden", marginTop: "90px" }}
           >
-            <BlogDetailSection
-              data={data}
-              PortableTextComponent={PortableTextComponent}
-            />
+            <Themeitem>
+              <div
+                style={{
+                  position: "relative",
+                  width: "100%", // Adjust the width as needed
+                  height: "300px", // Adjust the height as needed
+                  overflow: "hidden", // Hide overflow to prevent the cover photo from overflowing
+                }}
+              >
+                <img
+                  src="https://res.cloudinary.com/dtvtphhsc/image/upload/v1693428898/Blog-Cover_tw63ut.gif" // Replace with your cover photo URL
+                  alt="Cover"
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover", // Ensure the cover photo fills the container
+                    borderRadius: "10px",
+                  }}
+                />
+
+                <div
+                  style={{
+                    position: "absolute",
+                    top: "10px", // Adjust the top positioning
+                    right: "10px", // Adjust the right positioning
+                    backgroundColor: "rgba(0, 0, 0, 0.5)",
+                    color: "white",
+                    padding: "5px 10px",
+                    borderRadius: "4px",
+                  }}
+                >
+                  {/* {tour.date} */}
+                </div>
+
+                {/* Circular Profile Picture */}
+                <img
+                  src="https://res.cloudinary.com/dtvtphhsc/image/upload/fl_immutable_cache.lossy.progressive/v1693673198/0948d54e-68a2-451c-ba15-ac683b0c0780_kclx5w.webp" // Replace with your profile picture URL
+                  style={{
+                    position: "absolute",
+                    bottom: "0",
+                    left: "50%",
+                    transform: "translateX(-50%)", // Center horizontally
+                    width: "200px", // Adjust the width as needed
+                    height: "200px", // Maintain a square aspect ratio
+                    borderRadius: "50%",
+                    border: "2px solid rgba(250, 47, 210, 0.696)",
+                  }}
+                  alt="Profile"
+                />
+              </div>
+
+              <br />
+
+              <Theme>
+                {" "}
+                <h1>{data.title}</h1>{" "}
+              </Theme>
+
+              <div className="QuillDescription">
+                <div className="QuillDescription" style={{ width: "100%" }}>
+                  <Theme>
+                    <p>
+                      <PortableText
+                        value={data.content}
+                        components={PortableTextComponent}
+                      />
+                    </p>
+                  </Theme>
+                  <img src={urlFor(data.postimg).url()} alt="Img" />
+                </div>
+              </div>
+            </Themeitem>
           </Grid>
         </Grid>{" "}
         <br></br>
